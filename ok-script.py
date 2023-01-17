a2a231108c743da178441e7d8ff97b51edade1fd
Luis Saturno <saturno.luis@gmail.com>
2023-01-17 02:10:01 -0300

    Add base script

---
+++ ok-script.py
@@ -0,0 +1,42 @@
+import subprocess
+
+# List of commands to be executed 
+commands = [
+
+        "ls -l",
+        "pwd",
+        "asdf",
+
+        "END"]
+
+print("\033[42m" + "Process started..." + "\033[0m" + "\n")
+
+# Start executing the list of commands 
+for command in commands:
+    if command == "END":
+        print("\033[42m" + "\nEnd of process!\n" + "\033[0m")
+        break
+
+    # Run the command
+    print ("\033[32m" + "Running command:" + "\033[0m", command, "...\n")
+    process = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    # When the command runs successfully
+    if process.returncode == 0:
+        # Show the output 
+        print(process.stdout)
+        print("\033[32m" + "OK" + "\033[0m\n")
+
+    # When something went wrong
+    else:
+        # Show the error...
+        print("\033[41m" + "Error:" + "\033[0m", process.stderr)
+
+        # And ask the user if they whish to continue
+        response = input("\033[33m" + "Continue executing the the script? (Y/n): " + "\033[0m")
+
+        if response.lower() != "y":
+            # If not, then stop the execution 
+            print("\033[41m" + "\nExecution stopped\n" + "\033[0m")
+            break
+
