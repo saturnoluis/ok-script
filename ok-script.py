c400b7ecf160c6394ccc6fab5776e2e88a36edc1
Luis Saturno <saturno.luis@gmail.com>
2023-05-05 11:37:47 -0300

    Update install media codecs step

--- ok-script.py
+++ ok-script.py
@@ -17,9 +17,8 @@ commands = [
     "sudo dnf update -y",
 
     ">>> Install media codecs",
-    "sudo dnf install gstreamer1-plugins-{bad-\\*,good-\\*,base} --exclude=gstreamer1-plugins-bad-free-devel -y",
-    "sudo dnf install gstreamer1-plugin-openh264 gstreamer1-libav -y",
-    "sudo dnf install lame\\* --exclude=lame-devel -y",
+    "sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y",
+    "sudo dnf install lame\* --exclude=lame-devel -y",
     "sudo dnf install ffmpeg ffmpeg-libs libva libva-utils -y",
     "sudo dnf group upgrade --with-optional Multimedia -y",
 
