900b84eb33f320d189c2da9d258ee1ed2c9991ba
Luis Saturno <saturno.luis@gmail.com>
2023-05-05 12:13:39 -0300

    Add allowerasing flag to media codecs install

--- ok-script.py
+++ ok-script.py
@@ -17,9 +17,9 @@ commands = [
     "sudo dnf update -y",
 
     ">>> Install media codecs",
-    "sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y",
-    "sudo dnf install lame\* --exclude=lame-devel -y",
-    "sudo dnf install ffmpeg ffmpeg-libs libva libva-utils -y",
+    "sudo dnf install gstreamer1-plugins-{bad-\\*,good-\\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y",
+    "sudo dnf install lame\\* --exclude=lame-devel -y",
+    "sudo dnf install ffmpeg ffmpeg-libs libva libva-utils --allowerasing -y",
     "sudo dnf group upgrade --with-optional Multimedia -y",
 
     ">>> Remove libreoffice dnf packages",
