48614b09e23bea57a43449a2fe6a0cf6c2909309
Luis Saturno <saturno.luis@gmail.com>
2023-09-08 15:17:36 -0300

    wip

--- ok-script.sh
+++ ok-script.sh
@@ -1,3 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 
-echo "Hello!"
+echo "Hello! I will setup your Fedora install."
+
+# System update
+echo "Enable RPM fusion? (y/n)"
+read input
+if [ "$input" = "y" ]; then
+	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+	sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+fi
+
+echo "Update system? (y/n)"
+read input
+if [ "$input" = "y" ]; then
+	sudo dnf update -y
+fi
+
+echo "All done!"
